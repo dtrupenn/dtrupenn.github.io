70f6106ccd9d342891f0a0432b88f98b66044907
Dan Trujillo <dtrujillo@appnexus.com>
2026-02-12 21:52:45 -0500

    Duedi init

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        duedi: resolve(__dirname, "duedi/index.html"),
+      },
+    },
+  },
 });
